056efeab49fe0dabd93a8ced62772b0e2cb54d0e
Caio Andrade <andrade@hey.com>
2022-02-14 11:55:25 -0600

    Add comments to wish example

--- day1/wish.rb
+++ day1/wish.rb
@@ -1,3 +1,7 @@
+# My time working in the company that had this in the system was miserable
+# because I couldn't justify refactoring it into something that didn't feel
+# like an ontological challenge to the universe.
+
 class Wish
   def wish?
     rand(0..1).zero?
@@ -7,7 +11,12 @@ end
 wish = Wish.new
 
 if wish.wish?
-  wish.do_wish_stuff
+  puts "do_wish_stuff"
 else
-  wish.do_non_wish_stuff
+  puts "do_non_wish_stuff"
 end
+
+# The Wish class essentially worked as factory for things that may or may not
+# be, abstractly speaking, an actual wish (in the actual case, an order). That
+# means that the factory logic should have been abstracted into another class
+# that returns a Wish or an Order instance according to the given parameters.
